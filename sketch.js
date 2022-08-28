2f9fb593b8197e506aefec60e45912bc1bfaad3b
JordonH03 <jordonhong03@gmail.com>
2022-08-27 19:32:17 -0500

    Add final comments

--- sketch.js
+++ sketch.js
@@ -1,45 +1,49 @@
+// Randomly determine the initial values for the offsets of the creature's shape
+let xOffset = Math.random() * 1000;
+let yOffset = Math.random() * 1000;
+let anchorOffset = Math.random() * 1000;
+let inc = 0.1; // above values will be incremented by 0.1 each frame
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  frameRate(25);
-  stroke(255,1);
-  // stroke(255);
-  // noStroke();
+  frameRate(50);
+  stroke(0,1);
   strokeWeight(1);
-  // noFill();
-  fill(255,5);
 }
 
-let x = Math.random() * 500;
-let y = Math.random() * 500;
-let anchor = Math.random() * 500;
-let inc = 0.1;
-
 function draw() {
+
+  // Draw the creature
   background(0);
+  drawCreature(x, y, anchor);
 
-  // draw the jellyfish bell
-  drawBell(x, y, anchor);
-
-
+  // Simulate the creature's movment by incrementing the values over time
   x += inc;
   y += inc;
   anchor += inc;
-  // noLoop();
 }
 
+/**
+ * This function draws the body of the creature. The general shape of the creature is composed of several lines drawn by the curve() function.
+ * The position and curvature of the creature is newly calculated each call, using the noise() function to ensure the shapes are random and 
+ * smooth.
+ * @param {Number} xOffset argument for the noise of the x-value of curve endpoints
+ * @param {Number} yOffset argument for the noise of the y-value of curve endpoints
+ * @param {Number} anchorOffset arguement for the nosie of the curve anchor points
+ */
+function drawCreature(xOffset, yOffset, anchorOffset) {
+  const centerX = noise(xOffset/100) * width;  // Randomize the center of the shapes
+  const centerY = noise(yOffset/100) * height; // to simulate movement
+  const radius = 200; // Radius of the curve points 
 
-function drawBell(xOffset, yOffset, anchorOffset) {
+  // Draw the shape
+  for (let angle = 0; angle <= 270; angle += 0.1) {
 
-  const centerX = width / 2;
-  const centerY = height / 2;
-  const radius = 200;
-
-
-  for (let angle = 0; angle <= 180; angle += 0.1) {
-    const anchorRadius = noise(anchorOffset) * 400 + 600;   // get two random radii for the anchorpoints
-    const anchorRadius2 = noise(anchorOffset) * 400 + 600;    // between 600-1000, and 400-800
+    // Generate radii for the anchor points between 500-900
+    const anchorRadius = noise(anchorOffset) * 400 + 500;
+    const anchorRadius2 = noise(anchorOffset) * 400 + 500;
     
-    // generate noise
+    // Generate random offsets for the points
     const anchorNoise = noise(radians(angle), xOffset/500) * 720 - 360;
     const anchorNoise2 = noise(radians(angle), yOffset/500) * 720 - 360;
     const noiseX = noise(xOffset/200) * 400 - 200;
@@ -47,7 +51,7 @@ function drawBell(xOffset, yOffset, anchorOffset) {
     const noiseX2 = noise(xOffset/200) * 200 - 100;
     const noiseY2 = noise(yOffset/200) * 200 - 100;
 
-    // set position of curve anchor points
+    // Set position of curve anchor points
     const anchor1 = {
       x:centerX + anchorRadius * cos(anchorNoise), 
       y:centerY + anchorRadius2 * sin(anchorNoise)
@@ -57,7 +61,7 @@ function drawBell(xOffset, yOffset, anchorOffset) {
       y: centerY + anchorRadius2 * sin(anchorNoise2)
     };
 
-    // set position of curve endpoints
+    // Set position of curve endpoints
     const p1 = {
       x: centerX + radius * cos(radians(angle)+noiseX)+noiseX2, 
       y:centerY + radius * sin(radians(angle)+noiseX)+noiseY2
@@ -67,15 +71,14 @@ function drawBell(xOffset, yOffset, anchorOffset) {
       y: centerY + radius * sin(radians(angle+180)+noiseY)+noiseY2
     };
 
-    // stroke(255)
-    curve(anchor1.x, anchor1.y, p1.x, p1.y, p2.x, p2.y, anchor2.x, anchor2.y); // Draw a single curve
 
-    // circle(anchor1.x, anchor1.y, 10);
-    // circle(anchor2.x, anchor2.y, 10);
-
-    // stroke(0,0,255)
-    // line(anchor1.x, anchor1.y, p1.x, p1.y);
-    // line(anchor2.x, anchor2.y, p2.x, p2.y);
-    
+    // Draw the curve
+    fill(
+      noise(radians(angle))*100+150,
+      noise(radians(angle))*100+150,
+      noise(radians(angle))*100+150,
+      15
+    );
+    curve(anchor1.x, anchor1.y, p1.x, p1.y, p2.x, p2.y, anchor2.x, anchor2.y);
   }
 }
